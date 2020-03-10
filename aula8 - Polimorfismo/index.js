39e33432c94e8a077aef48473ff240993a6562a2
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-03-10 15:21:35 -0300

    aula 10/03 finaliza sessão 6

---
+++ aula8 - Polimorfismo/index.js
@@ -0,0 +1,48 @@
+// Superclasse
+function Conta(agencia, conta, saldo) {
+    this.agencia = agencia,
+    this.conta = conta,
+    this.saldo = saldo
+}
+
+Conta.prototype.sacar = function (valor) {
+    if(this.saldo < valor){
+        console.log("Saldo insuficiente");
+        return;
+    }
+        this.saldo -= valor
+        this.verSaldo();
+};
+
+Conta.prototype.depositar = function(valor) {
+    this.saldo += valor;
+    this.verSaldo();
+}
+
+Conta.prototype.verSaldo = function() {
+    console.log(`Ag/c: ${this.agencia}/${this.conta}` + ` Saldo: R$${this.saldo.toFixed(2)}`)
+}
+
+// const conta1 = new Conta(11 , 22, 10);
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function (valor) {
+    if(valor> (this.saldo + this.limite)){
+        console.log("Saldo insuficiente");
+        return;
+    }
+        this.saldo -= valor
+        this.verSaldo();
+};
+
+const cc = new ContaCorrente(11, 22 , 0 , 100)
+cc.depositar(10)
+cc.sacar(20)
+
